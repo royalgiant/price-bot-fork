3e0b80901aa761f7aafffc92494c551b3b005d1a
Donald Lee <donaldlee50@gmail.com>
2021-12-23 17:23:14 -0500

    20. Index2.js Refactoring & Working Deploy Scripts for Kovan
    We'll be doing more refactoring on index2.js and writing deploy scripts for kovan that works.

--- scripts/deploy_kovan_aave.js
+++ scripts/deploy_kovan_aave.js
@@ -6,12 +6,9 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const AaveV2FlashLoan = await ethers.getContractFactory("AaveV2FlashLoan");
-  const UniswapTradeBot = await ethers.getContractFactory("UniswapTradeBot");
-  const aave_contract = await AaveV2FlashLoan.deploy('0x9aab3f75489902f3a48495025729a0af77d4b11e','0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F','0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5');
-  const us_contract = await UniswapTradeBot.deploy('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F');
-
+  const aave_contract = await AaveV2FlashLoan.deploy('0x9aab3f75489902f3a48495025729a0af77d4b11e','0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F','0x88757f2f99175387ab4c6a4b3067c77a695b0349');
+  
   console.log("Aave Contract Token address:", aave_contract.address);
-  console.log("Uniswap Token address:", us_contract.address);
 }
 
 main()
